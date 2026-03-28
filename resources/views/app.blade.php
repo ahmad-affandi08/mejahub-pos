84065709588151c5dae7369908879e6465f0376b
Ahmad Affandi <ahmadaffandy008@gmail.com>
2026-03-28 10:16:12 +0700

    Refactor code structure for improved readability and maintainability

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" href="{{ asset('assets/favicon/favicon.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('assets/favicon/favicon.png') }}">
 
     <title inertia>{{ config('app.name', 'Mejahub POS') }}</title>
 
